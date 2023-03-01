479cc97f755b5dd8171825ca357eb699ccdf5b10
Aleksei <aleksey.detkov@gmail.com>
2023-03-01 10:32:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleTutor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ConsoleTutor;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string myName;
+        myName = "Anna";
+        Console.WriteLine(myName);
+        Console.ReadKey();
+    }
+}
